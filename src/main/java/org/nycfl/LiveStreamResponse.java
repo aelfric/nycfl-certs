e711b14c99ebe000e34678a5bafe0cd4057fbabd
Frank Riccobono <F.Riccobono@gmail.com>
2021-05-22 21:49:59 -0400

    Add a few more properties to the object

--- src/main/java/org/nycfl/LiveStreamResponse.java
+++ src/main/java/org/nycfl/LiveStreamResponse.java
@@ -4,6 +4,7 @@ import com.google.api.services.youtube.model.LiveBroadcast;
 import com.google.api.services.youtube.model.LiveBroadcastSnippet;
 import com.google.api.services.youtube.model.LiveStream;
 
+@SuppressWarnings("CdiInjectionPointsInspection")
 public class LiveStreamResponse {
   public final String title;
   public final String startTime;
@@ -13,8 +14,20 @@ public class LiveStreamResponse {
   public final String streamStatus;
   public final String monitorHtml;
   public final String broadcastId;
+  public final String ingestionAddress;
+  public final String privacyStatus;
 
-  public LiveStreamResponse(String title, String startTime, String endTime, String streamKey, String status, String streamStatus, String monitorHtml, String broadcastId) {
+  public LiveStreamResponse(
+      String title,
+      String startTime,
+      String endTime,
+      String streamKey,
+      String status,
+      String streamStatus,
+      String monitorHtml,
+      String broadcastId,
+      String ingestionAddress,
+      String privacyStatus) {
     this.title = title;
     this.startTime = startTime;
     this.endTime = endTime;
@@ -23,13 +36,15 @@ public class LiveStreamResponse {
     this.streamStatus = streamStatus;
     this.monitorHtml = monitorHtml;
     this.broadcastId = broadcastId;
+    this.ingestionAddress = ingestionAddress;
+    this.privacyStatus = privacyStatus;
   }
 
-  public static Builder builder(){
+  public static Builder builder() {
     return new Builder();
   }
 
-  public static class Builder{
+  public static class Builder {
     public String title;
     public String startTime;
     public String endTime;
@@ -38,8 +53,10 @@ public class LiveStreamResponse {
     private String streamStatus;
     private String embedHtml;
     private String broadcastId;
+    private String ingestionAddress;
+    private String privacyStatus;
 
-    public LiveStreamResponse build(){
+    public LiveStreamResponse build() {
       return new LiveStreamResponse(
           title,
           startTime,
@@ -48,25 +65,30 @@ public class LiveStreamResponse {
           status,
           streamStatus,
           embedHtml,
-          broadcastId
+          broadcastId,
+          ingestionAddress,
+          privacyStatus
       );
     }
 
-    public Builder withLiveStream(LiveStream liveStream){
+    public Builder withLiveStream(LiveStream liveStream) {
       if (liveStream != null) {
         streamKey = liveStream.getCdn().getIngestionInfo().getStreamName();
+        ingestionAddress = liveStream.getCdn().getIngestionInfo().getIngestionAddress();
         streamStatus = liveStream.getStatus().getStreamStatus();
       }
       return this;
     }
-    public Builder withBroadcast(LiveBroadcast broadcast){
+
+    public Builder withBroadcast(LiveBroadcast broadcast) {
       this.broadcastId = broadcast.getId();
       final LiveBroadcastSnippet snippet = broadcast.getSnippet();
       this.title = snippet.getTitle();
-      this.startTime  = snippet.getScheduledStartTime().toStringRfc3339();
-      this.endTime  = snippet.getScheduledEndTime().toStringRfc3339();
+      this.startTime = snippet.getScheduledStartTime().toStringRfc3339();
+      this.endTime = snippet.getScheduledEndTime().toStringRfc3339();
       this.status = broadcast.getStatus().getLifeCycleStatus();
       this.embedHtml = broadcast.getContentDetails().getMonitorStream().getEmbedHtml();
+      privacyStatus = broadcast.getStatus().getPrivacyStatus();
       return this;
     }
   }
