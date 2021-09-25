5cd432caf041f7fc31efb36086988d74089e0e90
Frank Riccobono <F.Riccobono@gmail.com>
2021-09-25 13:10:13 -0400

    Use cloudfront to host static assets going forward

--- src/main/java/org/nycfl/certificates/S3SyncResource.java
+++ src/main/java/org/nycfl/certificates/S3SyncResource.java
@@ -1,10 +1,10 @@
 package org.nycfl.certificates;
 
+import org.apache.http.client.utils.URIBuilder;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3Utilities;
-import software.amazon.awssdk.services.s3.model.GetUrlRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 import software.amazon.awssdk.services.s3.model.S3Object;
@@ -15,16 +15,21 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Path("/s3")
-@RolesAllowed({"basicuser","superuser"})
+@RolesAllowed({"basicuser", "superuser"})
 public class S3SyncResource extends S3Resource {
     @Inject
     S3Client s3;
+
+    @ConfigProperty(name = "cloudfront.host")
+    String cloudfrontHost;
 
     @POST
     @Path("upload")
@@ -38,8 +43,8 @@ public class S3SyncResource extends S3Resource {
         }
 
         PutObjectResponse putResponse = s3.putObject(
-                buildPublicPutRequest(formData),
-                RequestBody.fromFile(uploadToTemp(formData.file)));
+            buildPublicPutRequest(formData),
+            RequestBody.fromFile(uploadToTemp(formData.file)));
         if (putResponse != null) {
             return Response.ok().status(Status.CREATED).build();
         } else {
@@ -50,24 +55,30 @@ public class S3SyncResource extends S3Resource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<PublicListing> listFiles() {
-        ListObjectsRequest listRequest = ListObjectsRequest.builder().bucket(bucketName).build();
+        ListObjectsRequest
+            listRequest =
+            ListObjectsRequest.builder().bucket(bucketName).build();
         //HEAD S3 objects to get metadata
         return s3.listObjects(listRequest)
             .contents()
             .stream()
             .sorted(Comparator.comparing(S3Object::lastModified).reversed())
-            .map(o->getPublicListing(o.key()))
+            .map(o -> getPublicListing(o.key()))
             .collect(Collectors.toList());
     }
 
     private PublicListing getPublicListing(String objectName) {
-        S3Utilities utilities = s3.utilities();
-        GetUrlRequest request = GetUrlRequest.builder()
-            .bucket(bucketName)
-            .key(objectName)
-            .build();
-        URL url = utilities.getUrl(request);
-        return new PublicListing(url, objectName);
+        try {
+            URIBuilder builder = new URIBuilder();
+            builder.setScheme("http");
+            builder.setHost(cloudfrontHost);
+            builder.setPath(objectName);
+
+            URL url = builder.build().toURL();
+            return new PublicListing(url, objectName);
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new InternalServerErrorException("Could not construct resource URL", e);
+        }
     }
 
     public static class PublicListing {
